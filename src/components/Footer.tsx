ff23eda2f371ec6c2f38c20f6c30f244bde627b5
cb-here <cbv934@gmail.com>
2025-08-24 12:51:55 +0530

    update: added the heart emoji

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
   const getCurrentYear = () => new Date().getFullYear();
-  
+
   return (
     <footer className="text-gray-500 font-md flex items-center justify-center pb-5 pt-8">
       <div>
         <div className="flex justify-center">
-          <h1>Made with Love by cbhere </h1>
+          <h1>
+            Made with <span style={{ color: "red;" }}>❤️</span> by cbhere
+          </h1>
         </div>
         <div className="justify-center">
           <p>© {getCurrentYear()} cbhere. All rights reserved.</p>
